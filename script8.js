7718ec0944435b18f8d0d8ca506778719b61820e
Igor Małyszko <igor.malyszko33@gmail.com>
2024-09-19 19:07:29 +0200

    innit
    ppp

---
+++ script8.js
@@ -0,0 +1,6 @@
+//Zadanie 2
+//Ten kod prawidłowo parsuje poprawny JSON i wyświetla obiekt w konsoli
+
+const json = '{"name":"Mango","age":3,"isHappy":true}';
+const object = JSON.parse(json);
+console.log(obj);
---
+++ script82.js
@@ -0,0 +1,12 @@
+//Zadanie 3
+//ten kod przechwytuje błąd wywołany przez niepoprawny JSON, 
+//wyświetlając w konsoli szczegóły błędu (nazwę i wiadomość).
+
+const invalidJson = '{"name":"Mango","age":3,"isHappy":true}}';
+try {
+    const obj = JSON.parse(invalidJson);
+    console.log(obj);
+} catch (error) {
+    console.error("Name:", error.name);
+    console.error("Message:", error.message);
+}
